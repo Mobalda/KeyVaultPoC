6239b67ef74e63aff8e5eeb6c4842677abf5181c
Tuan Nguyen <tun@microsoft.com>
2017-03-02 22:40:31 -0500

    Initial commit

---
+++ src/KeyVaultPoC/Controllers/KeyVaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace KeyVaultPoC.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeyVaultController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public KeyVaultController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _configuration["ScoobyAPIKey"];
+        }
+    }
+}
